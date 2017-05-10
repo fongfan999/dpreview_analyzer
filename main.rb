9939dcbf7403a06bc6da93c753475aba9517d8eb
The Tiến <tiendt2311@gmail.com>
2017-05-10 20:52:44 +0700

    main

--- main.rb
+++ main.rb
@@ -1,12 +1,14 @@
 require_relative 'lib/dpreview/scraper'
-
-# Please pick year from https://www.dpreview.com/products/cameras/all?view=list
-
-scraper = Dpreview::Scraper.new('2016')
-scraper.scrape_amazon_reviews do |size, index|
-  puts "#{index + 1} - #{size}"
-end
-scraper.save
-
-`git add-commit -m "Update data 2016"`
-
+year_get = 2008
+# Please pick year_get from https://www.dpreview.com/products/cameras/all?view=list
+loop { 
+  puts year_get
+  scraper = Dpreview::Scraper.new(year_get)
+  scraper.scrape_amazon_reviews do |size, index|
+    puts "#{index + 1} - #{size}"
+  end
+  scraper.save
+  year_get -= 1
+  break if year_get == 1993
+  `git add-commit -m "Update data"`
+}
