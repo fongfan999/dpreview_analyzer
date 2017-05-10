d7f26d777e239eba5e43ee02233d7ad3c3130587
Phong Phan <fongfan999@gmail.com>
2017-05-10 19:57:48 +0700

    Update data 2014

--- main.rb
+++ main.rb
@@ -2,12 +2,12 @@ require_relative 'lib/dpreview/scraper'
 
 # Please pick year from https://www.dpreview.com/products/cameras/all?view=list
 
-scraper = Dpreview::Scraper.new('2015')
+scraper = Dpreview::Scraper.new('2014')
 scraper.scrape_amazon_reviews do |size, index|
   puts "#{index + 1} - #{size}"
 end
 scraper.save
 
-`git add-commit -m "Update data 2015"`
+`git add-commit -m "Update data 2014"`
 `git push`
 
