56d2b19f495e8f02dedc66ea116c7888c8ac15c7
Phong Phan <fongfan999@gmail.com>
2017-05-11 08:00:48 +0700

    Update data 2011

--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
 require_relative 'lib/dpreview/scraper'
 
 # Please pick year from https://www.dpreview.com/products/cameras/all?view=list
-year = 2012
+year = 2011
 
 scraper = Dpreview::Scraper.new(year)
 scraper.scrape_amazon_reviews do |size, index|
